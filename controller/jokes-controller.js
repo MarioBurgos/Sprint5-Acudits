4ec26721692f54e2e7b36600af5b743a4f683042
Mario Burgos <mario.burpi@gmail.com>
2021-10-25 12:20:41 +0200

    solved the bug with CORS by using a Proxy, but now the header content-type is set to text/html and I can't change it

--- controller/jokes-controller.js
+++ controller/jokes-controller.js
@@ -1,18 +1,79 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var Joke = /** @class */ (function () {
-    function Joke(id, joke, status) {
+    function Joke(id, string, status) {
         this.id = id;
-        this.joke = joke;
+        this.string = string;
         this.status = status;
     }
+    Joke.prototype.fetchAJoke = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var api, proxy, url, headers;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        api = "https://icanhazdadjoke.com/";
+                        proxy = "https://api.allorigins.win/get?url=";
+                        url = "" + proxy + encodeURIComponent(api);
+                        headers = new Headers();
+                        headers.append('Content-Type', 'application/json');
+                        headers.append('Accept', 'application/json');
+                        return [4 /*yield*/, fetch(url, { method: "GET", headers: headers })
+                                .then(function (response) { return console.log(response.text()); })
+                                .then(function (data) { return console.log(data); })["catch"](function (error) { return console.error(error); })];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
     return Joke;
 }());
 var UI = /** @class */ (function () {
     function UI() {
     }
-    UI.prototype.showRandomJoke = function (data) {
+    UI.prototype.showJoke = function (data) {
         var app = document.querySelector('#app');
         var div = document.createElement('div');
-        div.innerHTML = "\n        <div class=\"card w-75 m-auto px-3 py-2 shadow\">\n            <p class=\"text-start\">" + data.joke + "</p>\n        </div>\n        ";
+        if (app.hasChildNodes) {
+            app.firstChild.remove();
+        }
+        div.innerHTML = "\n        <div class=\"card w-75 m-auto px-3 py-2 shadow\">\n            <p class=\"text-start\"> " + data.string + "</p>\n        </div>\n        ";
         app.appendChild(div);
     };
     return UI;
@@ -21,15 +82,15 @@ var UI = /** @class */ (function () {
 document.querySelector('#action-button')
     .addEventListener('click', function (e) {
     var ui = new UI();
-    var joke = new Joke("ddd", "this is a joke", 400);
-    // fetch("https://icanhazdadjoke.com/", { mode: 'no-cors', headers: { Accept: 'application/json' } })
-    fetch("http://api.openweathermap.org/data/2.5/weather?appid=b0147f6411b11c4795a9f9e4bebc27a3&units=metric&q=copacabana", { mode: 'no-cors', headers: { 'Accept': 'application/json' } })
-        .then(function (response) {
-        console.log(response.json);
-        ui.showRandomJoke(joke);
-        return response.json;
-    })
-        .then(function (data) {
-        console.log(data);
-    })["catch"](function (error) { return console.error(error); });
+    var joke = new Joke("id", "this is a joke", 0);
+    // joke.fetchAJoke()
+    var proxy = "https://api.allorigins.win/get?url="; // https://allorigins.win/
+    var api = proxy + "https://icanhazdadjoke.com/";
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    fetch(api, { method: "GET", headers: headers })
+        .then(function (response) { return response.json(); })
+        .then(function (data) { return console.log(data); })["catch"](function (error) { return console.error(error); });
+    // ui.showJoke(joke);
 });
--- controller/jokes-controller.ts
+++ controller/jokes-controller.ts
@@ -1,51 +1,66 @@
 class Joke {
     id: string;
-    joke: string;
+    string: string;
     status: number;
 
-    constructor(id: string, joke: string, status: number) {
+    constructor(id: string, string: string, status: number) {
         this.id = id;
-        this.joke = joke;
+        this.string = string;
         this.status = status;
+    }
+
+    async fetchAJoke() {
+        // 
+        const api = `https://icanhazdadjoke.com/`;
+        const proxy = "https://api.allorigins.win/get?url=";  // https://allorigins.win/
+        const url = `${proxy}${encodeURIComponent(api)}`;
+
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('Accept', 'application/json');
+
+        await fetch(url, { method: "GET", headers: headers })
+            .then(response => console.log(response.text()))
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+
     }
 }
 
 class UI {
-    showRandomJoke(data: Joke) {
+    showJoke(data: Joke) {
         const app = document.querySelector('#app');
         const div = document.createElement('div');
+        if (app.hasChildNodes) {
+            app.firstChild.remove();
+        }
         div.innerHTML = `
         <div class="card w-75 m-auto px-3 py-2 shadow">
-            <p class="text-start">${data.joke}</p>
+            <p class="text-start"> ${data.string}</p>
         </div>
         `;
         app.appendChild(div);
-
     }
 }
-
-
 
 // DOM Events
 document.querySelector('#action-button')
     .addEventListener('click', (e) => {
         const ui = new UI();
-        let joke = new Joke("ddd", "this is a joke", 400);
+        let joke = new Joke("id", "this is a joke", 0);
+        // joke.fetchAJoke()
+        const proxy = "https://api.allorigins.win/get?url=";  // https://allorigins.win/
+        const api = `${proxy}https://icanhazdadjoke.com/`;
 
-        // fetch("https://icanhazdadjoke.com/", { mode: 'no-cors', headers: { Accept: 'application/json' } })
-        fetch("http://api.openweathermap.org/data/2.5/weather?appid=b0147f6411b11c4795a9f9e4bebc27a3&units=metric&q=copacabana", { mode: 'no-cors', headers: { 'Accept': 'application/json' } })
-            .then(response => {
-                console.log(response.json);
-                ui.showRandomJoke(joke);
-                return response.json
-            })
-            .then(data => {
-                console.log(data);
-            })
+
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('Accept', 'application/json');
+
+        fetch(api, { method: "GET", headers: headers})
+            .then(response => response.json())
+            .then(data => console.log(data))
             .catch(error => console.error(error));
+        // ui.showJoke(joke);
     });
-
-
-
-
 
